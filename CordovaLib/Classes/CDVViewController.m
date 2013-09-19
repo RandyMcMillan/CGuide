f870b70945c03968422dd51e3a557e7a752f58b5
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-09-19 17:36:24 -0400

    modified:   CordovaLib/Classes/CDVViewController.m 	modified:   c_guide/css/index.css

--- CordovaLib/Classes/CDVViewController.m
+++ CordovaLib/Classes/CDVViewController.m
@@ -280,12 +280,12 @@
     }
 
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGFloat screenWidth = screenRect.size.width-10;
-    CGFloat screenHeight = screenRect.size.height-25;
+    CGFloat screenWidth = screenRect.size.width/2.1;
+    CGFloat screenHeight = screenRect.size.height/2.1;
     [self.view setBackgroundColor:[UIColor colorWithRed:0.604 green:0.604 blue:0.604 alpha:1.000]];
     [self.webView setBackgroundColor:[UIColor colorWithRed:0.604 green:0.604 blue:0.604 alpha:1.000]];
     [self.webView setFrame:CGRectMake(5, 20, screenWidth, screenHeight)];
-    self.webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin);
+    //self.webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight);//|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin);
     self.webView.scalesPageToFit = [enableViewportScale boolValue];
 
     /*
