f2c30103e4cd895ee367f8f61983624aee08e0f7
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-03 18:08:04 -0400

    Changes to be committed: 	modified:   c_guide/config.codekit modified:   c_guide/css/index.css 	modified:   c_guide/index.html 	new file:   c_guide/js/addClasses.js 	new file:   c_guide/js/min/addClasses-min.js 	new file:   config.codekit

---
+++ c_guide/js/addClasses.js
@@ -0,0 +1,3 @@
+var z = document.body;
+document.body.classList.add('base03bg');
+document.b.classList.add('base02bg');
---
+++ c_guide/js/min/addClasses-min.js
@@ -0,0 +1,1 @@
+var z=document.body;document.body.classList.add("base03bg"),document.b.classList.add("base02bg");
