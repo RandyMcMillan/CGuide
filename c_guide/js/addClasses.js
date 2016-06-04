db6cfb36727bcdcbcf445bf82f29281885bcf60b
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-04 00:24:10 -0400

    Changes to be committed: 	modified:   c_guide/js/addClasses.js 	modified:   c_guide/js/min/addClasses-min.js

--- c_guide/js/addClasses.js
+++ c_guide/js/addClasses.js
@@ -1,97 +1,97 @@
 $(function(){
   $('body').each(function(){
-    $(this).addClass('base3bg');
+    $(this).addClass('base03bg');
   });
 });
 
-$('body').attr( 'text', '#657b83' );
+$('body').attr( 'text', '#839496' );
 
 $(function(){
   $('center').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('h1').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 
 $(function(){
   $('i').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('br').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('hr').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('pre').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('b').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 $(function(){
   $('a').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base1');
   });
 });
 
 $(function(){
   $('font').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('p').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('table').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('tbody').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('tr').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('td').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
 
 $(function(){
   $('code').each(function(){
-    $(this).addClass('base00');
+    $(this).addClass('base0');
   });
 });
