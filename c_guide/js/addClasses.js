0ec0c58ce2b3acc0d40454b65a2d5bd7b526a43b
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-03 22:17:38 -0400

    Changes to be committed: 	modified:   1.1.html 	modified:   1.2.html 	modified:   1.3.html 	modified:   1.4.html 	modified:   1.5.html 	modified:   1.6.html 	modified:   1.7.html 	modified:   2.1.html 	modified:   2.10.html 	modified:   2.11.html 	modified:   2.12.html 	modified:   2.13.html 	modified:   2.14.html 	modified:   2.15.html 	modified:   2.2.html 	modified:   2.3.html 	modified:   2.4.html 	modified:   2.5.html 	modified:   2.6.html 	modified:   2.7.html 	modified:   2.8.html 	modified:   2.9.html 	modified:   a.html 	modified:   css/index.css 	modified:   index2.html 	modified:   introduction.html 	modified:   js/addClasses.js 	modified:   js/min/addClasses-min.js 	modified:   template.html 	modified:   ../config.codekit

--- c_guide/js/addClasses.js
+++ c_guide/js/addClasses.js
@@ -1,3 +1,50 @@
-var z = document.body;
-document.body.classList.add('base03bg');
-document.b.classList.add('base02bg');
+var body = document.querySelector("body");
+body.classList.add("Base3bg");
+
+var center = document.querySelector("center");
+center.classList.add("base1");
+
+var h1 = document.querySelector("h1");
+h1.classList.add("base1");
+
+//var h2 = document.querySelector("h2");
+//h2.classList.add("base01");
+
+//var h3 = document.querySelector("h3");
+//h3.classList.add("base01");
+
+//var h4 = document.querySelector("h4");
+//h4.classList.add("base01");
+
+var hr = document.querySelector("hr");
+hr.classList.add("base1");
+
+var pre = document.querySelector("pre");
+pre.classList.add("base2bg");
+
+var a = document.querySelector("a");
+a.classList.add("base01");
+
+var ahref = document.querySelector("a.href");
+a.href.classList.add("sred");
+
+var b = document.querySelector("b");
+b.classList.add("sblue");
+
+var i = document.querySelector("i");
+i.classList.add("base00");
+
+var p = document.querySelector("p");
+p.classList.add("base00");
+
+var pre = document.querySelector("pre");
+pre.classList.add("base00");
+
+var blockquote = document.querySelector("blockquote");
+blockquote.classList.add("base00");
+
+var code = document.querySelector("code");
+code.classList.add("Base02");
+
+var td = document.querySelector("td");
+td.classList.add("sblue");
